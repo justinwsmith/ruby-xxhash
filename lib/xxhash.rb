a4be6a39a153aa7245e37a8f506d04d8f1b685c7
Justin W Smith <justin.w.smith@gmail.com>
2014-03-11 00:14:59 -0400

    fix white space

--- lib/xxhash.rb
+++ lib/xxhash.rb
@@ -52,77 +52,77 @@ class XXhash
       mem_avail = @@mem_total_size - @memsize
 
       if(remaining < mem_avail)
-       @memory[@memsize, remaining] = bytes[p, remaining]
-       @memsize += remaining
-       break
-     end
+        @memory[@memsize, remaining] = bytes[p, remaining]
+        @memsize += remaining
+        break
+      end
 
-     @memory[@memsize, mem_avail] = bytes[p, mem_avail]
+      @memory[@memsize, mem_avail] = bytes[p, mem_avail]
 
-     i = 0
-     [:v1, :v2, :v3, :v4].each do |m|
-      p32 = uint32(
-        @memory[i] |
-        (@memory[i+1] << 8) |
-        (@memory[i+2] << 16) |
-        (@memory[i+3] << 24))
+      i = 0
+      [:v1, :v2, :v3, :v4].each do |m|
+        p32 = uint32(
+          @memory[i] |
+          (@memory[i+1] << 8) |
+          (@memory[i+2] << 16) |
+          (@memory[i+3] << 24))
 
-      v = uint32(self.send(m) + p32 * @@prime32_2)
-      v = uint32(uint32((v << 13) | (v >> (32 - 13))) * @@prime32_1)
-      self.send((m.to_s + "=").to_sym, v)
-      i += 4
+        v = uint32(self.send(m) + p32 * @@prime32_2)
+        v = uint32(uint32((v << 13) | (v >> (32 - 13))) * @@prime32_1)
+        self.send((m.to_s + "=").to_sym, v)
+        i += 4
+      end
+
+      p += mem_avail
+      @memsize = 0
     end
 
-    p += mem_avail
-    @memsize = 0
+    return true
   end
 
-  return true
-end
+  def sum32
+    if @total_len >= 16
+      h32 = ((@v1 << 1) | (@v1 >> (32 - 1))) +
+      ((@v2 << 7) | (@v2 >> (32 - 7))) +
+      ((@v3 << 12) | (@v3 >> (32 - 12))) +
+      ((@v4 << 18) | (@v4 >> (32 - 18)))
+    else
+      h32 = @seed + @@prime32_5
+    end
 
-def sum32
-  if @total_len >= 16
-    h32 = ((@v1 << 1) | (@v1 >> (32 - 1))) +
-    ((@v2 << 7) | (@v2 >> (32 - 7))) +
-    ((@v3 << 12) | (@v3 >> (32 - 12))) +
-    ((@v4 << 18) | (@v4 >> (32 - 18)))
-  else
-    h32 = @seed + @@prime32_5
+    h32 = uint32(h32 + @total_len)
+
+    p = 0
+    while p <= (@memsize - 4)
+      p32 = uint32(@memory[p] |
+        (@memory[p+1] << 8) |
+        (@memory[p+2] << 16) |
+        (@memory[p+3] << 24))
+      h32 = uint32(h32 + p32 * @@prime32_3)
+      h32 = uint32(uint32((h32 << 17) | (h32 >> (32 - 17))) * @@prime32_4)
+      p += 4
+    end
+
+    while p < @memsize
+      h32 = uint32(h32 + @memory[p] * @@prime32_5)
+      h32 = uint32(uint32((h32 << 11) | (h32 >> (32 - 11))) * @@prime32_1)
+      p += 1
+    end
+
+    h32 ^= h32 >> 15
+    h32 = uint32(h32 * @@prime32_2)
+    h32 ^= h32 >> 13
+    h32 = uint32(h32 * @@prime32_3)
+    h32 ^= h32 >> 16
+
+    h32
   end
-
-  h32 = uint32(h32 + @total_len)
-
-  p = 0
-  while p <= (@memsize - 4)
-    p32 = uint32(@memory[p] |
-      (@memory[p+1] << 8) |
-      (@memory[p+2] << 16) |
-      (@memory[p+3] << 24))
-    h32 = uint32(h32 + p32 * @@prime32_3)
-    h32 = uint32(uint32((h32 << 17) | (h32 >> (32 - 17))) * @@prime32_4)
-    p += 4
-  end
-
-  while p < @memsize
-    h32 = uint32(h32 + @memory[p] * @@prime32_5)
-    h32 = uint32(uint32((h32 << 11) | (h32 >> (32 - 11))) * @@prime32_1)
-    p += 1
-  end
-
-  h32 ^= h32 >> 15
-  h32 = uint32(h32 * @@prime32_2)
-  h32 ^= h32 >> 13
-  h32 = uint32(h32 * @@prime32_3)
-  h32 ^= h32 >> 16
-
-  h32
-end
 
 private
 
-attr_accessor :v1, :v2, :v3, :v4
+  attr_accessor :v1, :v2, :v3, :v4
 
-def uint32(x)
-  x & @@thirtytwo1s
-end
+  def uint32(x)
+    x & @@thirtytwo1s
+  end
 end
