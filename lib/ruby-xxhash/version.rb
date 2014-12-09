b8d8342e4700f30102acf5537a504198d6f98338
Justin Smith <justin.smith@ge.com>
2014-12-08 20:13:05 -0500

    Added tests for 64-bit

--- lib/ruby-xxhash/version.rb
+++ lib/ruby-xxhash/version.rb
@@ -1,3 +1,3 @@
 class XXhash
-  VERSION = "0.1.1"
+  VERSION = "0.2.0"
 end
--- spec/xxhash_spec.rb
+++ spec/xxhash_spec.rb
@@ -6,30 +6,52 @@ require 'stringio'
 require 'yaml'
 
 describe XXhash do
-  hash = YAML.load(IO.read "spec/results.yaml")
+  hash32 = YAML.load(IO.read "spec/results32.yaml")
+  hash64 = YAML.load(IO.read "spec/results64.yaml")
 
-  hash.each do |key, value|
+  hash32.each do |key, value|
     it 'returns correct hash' do
-      XXhash.xxh32(key[0], key[1]).should eq(value)
+      expect(XXhash.xxh32(key[0], key[1])).to eq(value)
     end
   end
 
+  hash64.each do |key, value|
+    it 'returns correct hash' do
+      expect(XXhash.xxh64(key[0], key[1])).to eq(value)
+    end
+  end
 
   describe 'StreamingHash' do
     it 'raises error if argument is not an IO object' do
       expect { XXhash.xxh32_stream('test', 123)}.to raise_error
     end
 
-    hash.each do |key, value|
+    hash32.each do |key, value|
       it 'returns correct hash' do
-        XXhash.xxh32_stream(StringIO.new(key[0]), key[1]).should eq(value)
+        expect(XXhash.xxh32_stream(StringIO.new(key[0]), key[1])).to eq(value)
       end
     end
 
     it 'returns same hash for streamed files' do
       h1 = XXhash.xxh32(File.read(__FILE__), 123)
       h2 = XXhash.xxh32_stream(File.open(__FILE__), 123)
-      h1.should eq(h2)
+      expect(h1).to eq(h2)
+    end
+
+    it 'raises error if argument is not an IO object' do
+      expect { XXhash.xxh64_stream('test', 123)}.to raise_error
+    end
+
+    hash64.each do |key, value|
+      it 'returns correct hash' do
+        expect(XXhash.xxh64_stream(StringIO.new(key[0]), key[1])).to eq(value)
+      end
+    end
+
+    it 'returns same hash for streamed files' do
+      h1 = XXhash.xxh64(File.read(__FILE__), 123)
+      h2 = XXhash.xxh64_stream(File.open(__FILE__), 123)
+      expect(h1).to eq(h2)
     end
   end
 end
